49ae28f4e721b5586b090607c6cee60a1bf10506
komkowvlad <komkowvald1998@gmail.com>
2016-11-28 23:07:08 +0800

    Vector
    class Vector

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Vector v = new Vector(2,4,3,5);
+        Vector v1 = new Vector(3,5,4,6);
+        v.equals(v1);
+        v.length();
+        System.out.println(v);
+    }
+}
---
+++ src/com/company/Vector.java
@@ -0,0 +1,43 @@
+package com.company;
+public class Vector {
+    int x1, y1, x2, y2;
+
+    public String toString(){
+        return "["+ x1 +","+y1+";"+x2+","+y2+  "]";
+    }
+    Vector(int X1, int Y1, int X2, int Y2){
+        x1 = X1; y1 = Y1; x2 = X2; y2 = Y2;
+    }
+    public int getX1() {
+        return x1;
+    }
+    public int getX2() {
+        return x2;
+    }
+    public int getY1() {
+        return y1;
+    }
+    public int getY2() {
+        return y2;
+    }
+
+    public void equals(Vector v){
+        int toX = v.getX1()- x1;
+        int toX1 = v.getX2()- x2;
+        int toY = v.getY1()- y1;
+        int toY1 = v.getY2()- y2;
+        if(toX == toX1){
+            if (toY == toY1){System.out.println("true");}
+            else {System.out.println("false");}
+        }
+        else {System.out.println("false");}
+    }
+
+    public void length(){
+        int x = x2-x1;
+        int y = y1-y2;
+        double l = Math.hypot(x,y);
+        System.out.println(l);
+    }
+
+}
