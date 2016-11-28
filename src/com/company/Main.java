6faf44f2979f0fec3d7124acd81fac574f89a47d
komkowvlad <komkowvald1998@gmail.com>
2016-11-28 23:34:21 +0800

    v
    vff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	    Vector v = new Vector(2,4,3,5);
         Vector v1 = new Vector(3,5,4,6);
-        v.equals(v1);
-        v.length();
         System.out.println(v);
         System.out.println(v.equals(v1));
         System.out.println(v.length());
